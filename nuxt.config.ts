0b16f6c1fee8750abef6134671f272d97f684610
brnkgabriel <brnkgabriel@gmail.com>
2023-05-08 16:22:10 +0100

    added origin and secret

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src',
+  secret: process.env.AUTH_SECRET,
+  auth: {
+    origin: process.env.AUTH_ORIGIN
+  },
   modules: [
     '@sidebase/nuxt-auth',
     '@nuxtjs/tailwindcss'
